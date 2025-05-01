066359ca7d871b5b16eb504d3dd3747c4463249f
Ezequiel <evilardo@fi.uba.ar>
2025-05-01 17:24:02 -0300

    config: migrations primeras tablas

---
+++ TP-ITIL-9559/Data/ITILDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TP_ITIL_9559.Data
+{
+    public class ITILDbContext : DbContext
+    {
+        public ITILDbContext(DbContextOptions<ITILDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.DisplayName());
+            }
+        }
+    }
+}
--- TP-ITIL-9559/Program.cs
+++ TP-ITIL-9559/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TP_ITIL_9559.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+
+var connectionString = builder.Configuration.GetConnectionString("MyPostgresConnection") ?? throw new InvalidOperationException("Connection string 'MyPostgresConnection' not found.");
+builder.Services.AddDbContext<ITILDbContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
