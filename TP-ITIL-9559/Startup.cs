86be20d2de3a40174bf74c6a8b528aad34506103
Ezequiel <evilardo@fi.uba.ar>
2025-05-01 21:58:14 -0300

    config

--- TP-ITIL-9559/Startup.cs
+++ TP-ITIL-9559/Startup.cs
@@ -22,34 +22,19 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ITILDbContext dbContext)
     {
-        // Use exception handling middleware to handle any exceptions thrown by the app
         app.UseExceptionHandler("/error");
-
-        // Use HSTS middleware to enforce HTTPS usage
         app.UseHsts();
-
-        // Use HTTPS redirection middleware to redirect all non-HTTPS requests to HTTPS
         app.UseHttpsRedirection();
-
-        // Use static file middleware to serve static files (e.g., HTML, CSS, JS) from the 'wwwroot' folder
         app.UseStaticFiles();
-
-        // Use routing middleware to map incoming requests to the appropriate action method
         app.UseRouting();
-
-        // Use authentication middleware to handle user authentication and authorization
         app.UseAuthentication();
-
-        // Use authorization middleware to apply authorization policies to the incoming requests
         app.UseAuthorization();
 
-        // Use endpoints middleware to map incoming requests to the appropriate controller action method
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
         });
 
-        // Migrate the database schema on startup (if necessary)
         dbContext.Database.Migrate();
     }
 
